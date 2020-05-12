c29fec8d7ba33f53c41d48c1ddae2ffdd2b26454
Carlos Andres <carloscampo5200@hotmail.com>
2020-05-12 01:07:49 -0500

    Android Basics / Class 4 Ep 13 Carlos

--- app/src/main/java/com/example/android/miwok/ColorsActivity.java
+++ app/src/main/java/com/example/android/miwok/ColorsActivity.java
@@ -32,21 +32,19 @@ public class ColorsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_colors);
+        setContentView(R.layout.word_list);
 
         // Create an ArrayList of words
         ArrayList<Word> words = new ArrayList<Word>();
 
-        words.add(new Word("one", "lutti"));
-        words.add(new Word("two", "otiiko"));
-        words.add(new Word("three", "tolookosu"));
-        words.add(new Word("four", "oyyisa"));
-        words.add(new Word("five", "massokka"));
-        words.add(new Word("six", "temmokka"));
-        words.add(new Word("seven", "kenekaku"));
-        words.add(new Word("eight", "kawinta"));
-        words.add(new Word("nine", "wo’e"));
-        words.add(new Word("ten", "na’aacha"));
+        words.add(new Word("red", "wetetti", R.drawable.color_red));
+        words.add(new Word("green", "chokokki", R.drawable.color_green));
+        words.add(new Word("brown", "takaakki", R.drawable.color_brown));
+        words.add(new Word("gray", "topoppi", R.drawable.color_gray));
+        words.add(new Word("black", "kululli", R.drawable.color_black));
+        words.add(new Word("white", "kelelli", R.drawable.color_white));
+        words.add(new Word("dusty yellow", "topiisa", R.drawable.color_dusty_yellow));
+        words.add(new Word("mustard yellow", "chiwiita", R.drawable.color_mustard_yellow));
 
         // Create an {@link WordAdapter}, whose data source is a list of {@link Word}s. The
         // adapter knows how to create list items for each item in the list.
@@ -54,7 +52,7 @@ public class ColorsActivity extends AppCompatActivity {
 
         // Find the {@link ListView} object in the view hierarchy of the {@link Activity}.
         // There should be a {@link ListView} with the view ID called list, which is declared in the
-        // activity_numbers.xml layout file.
+        // word_list.xml file.
         ListView listView = (ListView) findViewById(R.id.list);
 
         // Make the {@link ListView} use the {@link WordAdapter} we created above, so that the
--- app/src/main/java/com/example/android/miwok/PhrasesActivity.java
+++ app/src/main/java/com/example/android/miwok/PhrasesActivity.java
@@ -32,21 +32,21 @@ public class PhrasesActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_phrases);
+        setContentView(R.layout.word_list);
 
         // Create an ArrayList of words
         ArrayList<Word> words = new ArrayList<Word>();
 
-        words.add(new Word("one", "lutti"));
-        words.add(new Word("two", "otiiko"));
-        words.add(new Word("three", "tolookosu"));
-        words.add(new Word("four", "oyyisa"));
-        words.add(new Word("five", "massokka"));
-        words.add(new Word("six", "temmokka"));
-        words.add(new Word("seven", "kenekaku"));
-        words.add(new Word("eight", "kawinta"));
-        words.add(new Word("nine", "wo’e"));
-        words.add(new Word("ten", "na’aacha"));
+        words.add(new Word("Where are you going?", "minto wuksus", 0));
+        words.add(new Word("What is your name?", "tinna oyaase'na", 0));
+        words.add(new Word("My name is...", "oyaaset", 0));
+        words.add(new Word("How are you feeling?", "michaksas?", 0));
+        words.add(new Word("I'm feeling good", "kuchi achit", 0));
+        words.add(new Word("Are you coming?", "eenas'aa?",0 ));
+        words.add(new Word("Yes, I'm coming", "hee' eenam", 0));
+        words.add(new Word("I'm coming", "eenam", 0));
+        words.add(new Word("Let's go", "Yoowutis", 0));
+        words.add(new Word("Come here", "enni' nem", 0));
 
         // Create an {@link WordAdapter}, whose data source is a list of {@link Word}s. The
         // adapter knows how to create list items for each item in the list.
@@ -54,7 +54,7 @@ public class PhrasesActivity extends AppCompatActivity {
 
         // Find the {@link ListView} object in the view hierarchy of the {@link Activity}.
         // There should be a {@link ListView} with the view ID called list, which is declared in the
-        // activity_numbers.xml layout file.
+        // word_listyout file.
         ListView listView = (ListView) findViewById(R.id.list);
 
         // Make the {@link ListView} use the {@link WordAdapter} we created above, so that the
